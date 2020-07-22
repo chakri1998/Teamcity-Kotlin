5fc5304f03ef2728fac54ed95e5db048d0ae171b
chakri1998 <35056463+chakri1998@users.noreply.github.com>
2020-07-22 17:53:24 +0530

    ok

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,6 +5,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 
 
 /*
